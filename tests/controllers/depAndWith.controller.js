625cce8f46e55c6a3b028e7a5f918b73dce72290
Fernando-Olmedo <olmedo_pr@hotmail.com>
2022-07-22 01:05:40 -0300

    feat: (Req-Tests) - Creating unit tests for depAndWith.controller

---
+++ tests/controllers/depAndWith.controller.js
@@ -0,0 +1,45 @@
+const { expect } = require('chai');
+const sinon = require('sinon');
+const depAndWithController = require('../../src/controllers/depAndWith.controller');
+const depAndWithService = require('../../src/services/depAndWith.service');
+
+describe('Testa a camada depAndWithController', () => {
+  describe('Testa a rota /investimentos/comprar', () => {
+    describe('Testa o metodo POST', () => {
+      describe('Testa a função deposit', () => {
+        const req = {};
+        const res = {};
+        const contaObj = {
+          codCliente: 1,
+          saldo: 102,
+        };
+        const bodyObj = {
+            "codCliente": 1,
+            "codAtivo": 102,
+            "qtdAtivo": 100
+          };
+
+        before(() => {
+          req.body = bodyObj;
+          res.status = sinon.stub().returns(res);
+          res.json = sinon.stub().returns();
+          sinon.stub(depAndWithService, 'createMov').resolves(contaObj);
+        });
+
+        after(() => {
+          depAndWithService.createMov.restore();
+        });
+
+        it('Testa se STATUS é chamado com o código 201.', async () => {
+          await depAndWithController.deposit(req, res);
+          expect(res.status.calledWith(201)).to.be.equal(true);
+        });
+
+        it('Testa se JSON é chamado com o ativo correto.', async () => {
+          await depAndWithController.deposit(req, res);
+          expect(res.json.calledWith(contaObj)).to.be.equal(true);
+        });
+      });
+    });
+  });
+});
