6f69bc1df0acb6ee2b2aa7989e5f107b94195a60
Fernando-Olmedo <olmedo_pr@hotmail.com>
2022-07-22 17:08:33 -0300

    feat: (Req-Tests) - Creating unit tests for withdrawal function in depAndWith.controller

--- tests/controllers/depAndWith.controller.js
+++ tests/controllers/depAndWith.controller.js
@@ -4,32 +4,31 @@ const depAndWithController = require('../../src/controllers/depAndWith.controlle
 const depAndWithService = require('../../src/services/depAndWith.service');
 
 describe('Testa a camada depAndWithController', () => {
+  const req = {};
+  const res = {};
+  const contaObj = {
+    codCliente: 1,
+    saldo: 102,
+  };
+  const bodyObj = {
+    "codCliente": 1,
+    "codAtivo": 102,
+    "qtdAtivo": 100
+  };
+  before(() => {
+    req.body = bodyObj;
+    res.status = sinon.stub().returns(res);
+    res.json = sinon.stub().returns();
+    sinon.stub(depAndWithService, 'createMov').resolves(contaObj);
+  });
+
+  after(() => {
+    depAndWithService.createMov.restore();
+  });
+
   describe('Testa a rota /investimentos/comprar', () => {
     describe('Testa o metodo POST', () => {
       describe('Testa a função deposit', () => {
-        const req = {};
-        const res = {};
-        const contaObj = {
-          codCliente: 1,
-          saldo: 102,
-        };
-        const bodyObj = {
-            "codCliente": 1,
-            "codAtivo": 102,
-            "qtdAtivo": 100
-          };
-
-        before(() => {
-          req.body = bodyObj;
-          res.status = sinon.stub().returns(res);
-          res.json = sinon.stub().returns();
-          sinon.stub(depAndWithService, 'createMov').resolves(contaObj);
-        });
-
-        after(() => {
-          depAndWithService.createMov.restore();
-        });
-
         it('Testa se STATUS é chamado com o código 201.', async () => {
           await depAndWithController.deposit(req, res);
           expect(res.status.calledWith(201)).to.be.equal(true);
@@ -37,6 +36,21 @@ describe('Testa a camada depAndWithController', () => {
 
         it('Testa se JSON é chamado com o ativo correto.', async () => {
           await depAndWithController.deposit(req, res);
+          expect(res.json.calledWith(contaObj)).to.be.equal(true);
+        });
+      });
+    });
+  });
+  describe('Testa a rota /investimentos/vender', () => {
+    describe('Testa o metodo POST', () => {
+      describe('Testa a função withdrawal', () => {
+        it('Testa se STATUS é chamado com o código 201.', async () => {
+          await depAndWithController.withdrawal(req, res);
+          expect(res.status.calledWith(201)).to.be.equal(true);
+        });
+
+        it('Testa se JSON é chamado com o ativo correto.', async () => {
+          await depAndWithController.withdrawal(req, res);
           expect(res.json.calledWith(contaObj)).to.be.equal(true);
         });
       });
