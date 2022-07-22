e79cfd2ccf04ac3e8fc5da4eddaf33dfdf6d1334
Fernando-Olmedo <olmedo_pr@hotmail.com>
2022-07-22 18:22:02 -0300

    fix: (Req-Tests) - Correcting routes names in the deposit and withdrwals tests

--- tests/controllers/depAndWith.controller.js
+++ tests/controllers/depAndWith.controller.js
@@ -4,54 +4,59 @@ const depAndWithController = require('../../src/controllers/depAndWith.controlle
 const depAndWithService = require('../../src/services/depAndWith.service');
 
 describe('Testa a camada depAndWithController', () => {
-  const req = {};
-  const res = {};
-  const contaObj = {
-    codCliente: 1,
-    saldo: 102,
-  };
-  const bodyObj = {
-    "codCliente": 1,
-    "codAtivo": 102,
-    "qtdAtivo": 100
-  };
-  before(() => {
-    req.body = bodyObj;
-    res.status = sinon.stub().returns(res);
-    res.json = sinon.stub().returns();
-    sinon.stub(depAndWithService, 'createMov').resolves(contaObj);
+  describe('Testa a rota /conta/{codCliente}', () => {
+
   });
+  describe('Testa a rota /conta/(saque || deposito)', () => {
+    const req = {};
+    const res = {};
+    const contaObj = {
+      codCliente: 1,
+      saldo: 102,
+    };
+    const bodyObj = {
+      "codCliente": 1,
+      "codAtivo": 102,
+      "qtdAtivo": 100
+    };
+    before(() => {
+      req.body = bodyObj;
+      res.status = sinon.stub().returns(res);
+      res.json = sinon.stub().returns();
+      sinon.stub(depAndWithService, 'createMov').resolves(contaObj);
+    });
 
-  after(() => {
-    depAndWithService.createMov.restore();
-  });
+    after(() => {
+      depAndWithService.createMov.restore();
+    });
 
-  describe('Testa a rota /investimentos/comprar', () => {
-    describe('Testa o metodo POST', () => {
-      describe('Testa a função deposit', () => {
-        it('Testa se STATUS é chamado com o código 201.', async () => {
-          await depAndWithController.deposit(req, res);
-          expect(res.status.calledWith(201)).to.be.equal(true);
-        });
+    describe('Testa a rota /conta/deposito', () => {
+      describe('Testa o metodo POST', () => {
+        describe('Testa a função deposit', () => {
+          it('Testa se STATUS é chamado com o código 201.', async () => {
+            await depAndWithController.deposit(req, res);
+            expect(res.status.calledWith(201)).to.be.equal(true);
+          });
 
-        it('Testa se JSON é chamado com o ativo correto.', async () => {
-          await depAndWithController.deposit(req, res);
-          expect(res.json.calledWith(contaObj)).to.be.equal(true);
+          it('Testa se JSON é chamado com o ativo correto.', async () => {
+            await depAndWithController.deposit(req, res);
+            expect(res.json.calledWith(contaObj)).to.be.equal(true);
+          });
         });
       });
     });
-  });
-  describe('Testa a rota /investimentos/vender', () => {
-    describe('Testa o metodo POST', () => {
-      describe('Testa a função withdrawal', () => {
-        it('Testa se STATUS é chamado com o código 201.', async () => {
-          await depAndWithController.withdrawal(req, res);
-          expect(res.status.calledWith(201)).to.be.equal(true);
-        });
+    describe('Testa a rota /conta/saque', () => {
+      describe('Testa o metodo POST', () => {
+        describe('Testa a função withdrawal', () => {
+          it('Testa se STATUS é chamado com o código 201.', async () => {
+            await depAndWithController.withdrawal(req, res);
+            expect(res.status.calledWith(201)).to.be.equal(true);
+          });
 
-        it('Testa se JSON é chamado com o ativo correto.', async () => {
-          await depAndWithController.withdrawal(req, res);
-          expect(res.json.calledWith(contaObj)).to.be.equal(true);
+          it('Testa se JSON é chamado com o ativo correto.', async () => {
+            await depAndWithController.withdrawal(req, res);
+            expect(res.json.calledWith(contaObj)).to.be.equal(true);
+          });
         });
       });
     });
