f26e47954969b979f1b1051dd95a6a3bb97e9b8b
Fernando-Olmedo <olmedo_pr@hotmail.com>
2022-07-24 15:25:12 -0300

    test database plugin

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -2,8 +2,18 @@ require('dotenv').config();
 
 module.exports = {
   development: {
-    use_env_variable: "JAWSDB_URL",
+    username: process.env.MYSQL_USER || 'root',
+    password: process.env.MYSQL_PASSWORD || '1234',
+    database: 'XP_psel',
+    host: process.env.MYSQL_HOST || 'localhost',
+    port: process.env.MYSQL_PORT || '3306',
     dialect: 'mysql',
+    dialectOptions: {
+      decimalNumbers: true,
+    },
+    define: {
+      timestamps: false,
+  },
   },
   test: {
     username: process.env.MYSQL_USER || 'root',
