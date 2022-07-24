2cbb3e173de607d46803bb1b9028f5c92b2cd138
Fernando-Olmedo <olmedo_pr@hotmail.com>
2022-07-24 16:43:14 -0300

    test database plugin

--- src/database/models/index.js
+++ src/database/models/index.js
@@ -9,8 +9,8 @@ const config = require(__dirname + '/../config/config.js')[env];
 const db = {};
 
 let sequelize;
-if (config.envVar) {
-  sequelize = new Sequelize(process.env[config.envVar], config);
+if (config.env) {
+  sequelize = new Sequelize(process.env[env], config);
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
